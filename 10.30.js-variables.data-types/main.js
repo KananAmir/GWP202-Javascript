55aa681fcbd6470e022b49c305702854dd2e1a72
KananAmir <amirovknn@gmail.com>
2023-10-30 18:34:05 +0400

    js variables, data types

---
+++ 10.30.js-variables.data-types/main.js
@@ -0,0 +1,95 @@
+// var testVariable; // hoisting
+
+let className = "GWP202";
+
+let score = 85;
+
+// variable decleared and value assigned
+let isUserOnline = false;
+
+let userName = null;
+userName = "Nargiz";
+console.log("user name: ", userName);
+
+// variable decleared
+let cityName;
+
+// console.log(userName);
+console.log("City Name", cityName); // undefined
+
+console.log("Hello World!");
+
+// function sayHi() {}
+
+// console.log(sayHi()); //undefined
+
+//object sample
+
+let userObject = {
+  firstName: "Lorem",
+  lastName: "Ipsum",
+  age: 44,
+};
+
+let firstName, lastName;
+
+firstName = "Kanan";
+lastName = "Amirov";
+
+let number = "4444";
+console.log(number);
+
+number = "9999";
+
+console.log(number);
+
+// window.alert(firstName);
+
+// let const var
+
+// let studentName = "Aysel";
+
+// var global variable, function scope
+// let vs const local variable, block scope
+
+var studentInfo = "lorem ipsum";
+
+//block scope
+// {
+//   let studentName = "Sama";
+// }
+// console.log(studentName);
+
+// {
+//   var studentName = "Sama";
+// }
+// console.log(studentName);
+
+function testFunc() {
+  var user = "Sabina";
+  console.log(user);
+}
+
+// testFunc();
+
+// let uniniversity = "GDU";
+
+// let uniniversity = "BDU";
+
+// const BASE_URL = "www.test.com";
+
+// BASE_URL = "abc"; //error
+
+// variable name limitations
+// let, return, function, class
+
+// let let = "lorem ipsum"
+
+// console.log("test: ", testVariable); // error
+// let testVariable = "lorem ipsum dolor";
+
+console.log("test: ", testVariable);
+
+var testVariable = "lorem ipsum dolor";
+
+// console.log("salam");
---
+++ 10.30.js-variables.data-types/operators.js
@@ -0,0 +1,49 @@
+let firstNum, secondNum, sum, subtruct, multiply, division, reminder;
+
+firstNum = 15;
+secondNum = 6;
+
+//arichmetic operators
+sum = firstNum + secondNum;
+subtruct = firstNum - secondNum;
+multiply = firstNum * secondNum;
+division = firstNum / secondNum;
+reminder = firstNum % secondNum;
+let power = firstNum ** secondNum;
+
+console.log("Sum: ", sum);
+console.log("Subtruct: ", subtruct);
+console.log("Multiply: ", multiply);
+console.log("Division: ", division);
+console.log("Reminder: ", reminder);
+console.log("Power: ", power);
+
+let count = 5;
+
+// count = count + 1;
+
+// count++;
+// count--;
+
+// count = count + 5;
+// count += 5;
+
+// count -= 5;
+// count = count - 5;
+
+// count = count ** 2;
+count **= 2;
+console.log("Count: ", count);
+
+//Shift Assignment Operators
+
+console.log(-15 >> 1); //7
+console.log(15 << 1); //30
+console.log(15 << 2); //60
+
+// Bitwise Assignment Operators
+
+console.log(15 & 3); //3
+console.log(15 | 3); //15
+console.log(7 & 8); // 0
+console.log(7 | 8); //15
